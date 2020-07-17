7707f7b34faa61d1a6204466872642108423f69c
whinter-is-coming <68386914+whinter-is-coming@users.noreply.github.com>
2020-07-17 11:30:58 -0300

    Update config.js

--- configuration/config.js
+++ configuration/config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  facebook_api_key: "705516460231159",
-  facebook_api_secret: "c1f8691c5e0dce6b6d6a4aa8887a35c4",
+  facebook_api_key: "facebook-key",
+  facebook_api_secret: "facebook-key",
   callback_url: "http://localhost:3000/auth/facebook/callback",
   host: "localhost"
 };
